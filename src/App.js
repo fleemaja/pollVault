f25c6e8c917d5f753d359c7d625bd79b711fc1b4
Drew Fleeman <drew.fleeman@gmail.com>
2017-10-02 12:00:56 -0500

    add material ui appBar and Drawer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    drawerOpen: true
+  }
+
+  handleToggle = () => this.setState({drawerOpen: !this.state.drawerOpen})
+
   render() {
+    const appBarWidth = this.state.drawerOpen ? 'calc(100% - 256px)' : '100%';
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Poll Vault</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider className="App">
+
+        <AppBar
+          style={{backgroundColor: '#fff', width: appBarWidth}}
+          titleStyle={{color: '#333'}}
+          title="PollVault"
+          showMenuIconButton={false}
+          iconElementRight={<RaisedButton label="Toggle Filters" onClick={this.handleToggle.bind(this)} />}
+        />
+
+        <div>
+          <Drawer open={this.state.drawerOpen} openSecondary={true}>
+            <MenuItem>Menu Item</MenuItem>
+            <MenuItem>Menu Item 2</MenuItem>
+          </Drawer>
+        </div>
+
+      </MuiThemeProvider>
     );
   }
 }
