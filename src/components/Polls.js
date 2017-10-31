720309c724eb88432c8f891b5de23e7db6f77c67
Drew Fleeman <drew.fleeman@gmail.com>
2017-10-31 12:56:11 -0500

    no results found message for type ahead poll search

--- src/components/Polls.js
+++ src/components/Polls.js
@@ -11,16 +11,29 @@ const masonryOptions = {
 
 class Polls extends Component {
 
+  state = {
+    noResults: false
+  }
+
   componentWillMount = () => {
     this.props.getPolls();
+  }
+
+  componentWillReceiveProps = (nextProps) => {
+    if (nextProps.polls.length > 0) {
+      this.setState({ noResults: false })
+    } else {
+      this.setState({ noResults: true })
+    }
   }
 
   render() {
     const sortByKey = (sortKey) => (a, b) => a[sortKey] < b[sortKey];
     const category = this.props.category;
     const sortKey = this.props.sortKey;
+    const { noResults } = this.state;
     return (
-      <section style={{width: this.props.contentWidth, paddingTop: 40, backgroundColor: '#fcfdfe'}}>
+      <section style={{width: this.props.contentWidth, paddingTop: 40}}>
         <Masonry
           options={masonryOptions}
           style={{margin: '0 auto'}}>
@@ -29,6 +42,13 @@ class Polls extends Component {
               .filter(p => category === 'all' || p.category === category)
               .sort(sortByKey(sortKey))
               .map(p => <Poll poll={p} />)
+          }
+          {
+            noResults &&
+            <section style={{textAlign: 'center', color: '#555'}}>
+              <p style={{fontSize: 72, margin: 40}}>¯\_(ツ)_/¯</p>
+              <p>No results found!</p>
+            </section>
           }
         </Masonry>
       </section>
