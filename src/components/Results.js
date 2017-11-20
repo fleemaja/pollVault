afafff9ea467e3321afdc778dcf64183e547c01b
Drew Fleeman <drew.fleeman@gmail.com>
2017-11-20 16:25:28 -0600

    results animation on mount and on vote

--- src/components/Results.js
+++ src/components/Results.js
@@ -2,27 +2,44 @@ import React, { Component } from 'react';
 import CheckMark from 'material-ui/svg-icons/maps/beenhere';
 
 class Results extends Component {
+  state = {
+    mounted: false
+  }
+
+  componentDidMount = () => {
+    this.setState({ mounted: true })
+  }
+
   render() {
     const ipVote = this.props.ipVote
     const choices = this.props.choices
     const totalVotes = choices.reduce((accumulator, choice) => (
       accumulator + choice.votes
     ), 0);
+    const { mounted } = this.state
     return (
       <section>
         {
           choices.map(c => {
             const isUserVote = ipVote === c.id;
             const backgroundColor = isUserVote ? '#cdebf9' : '#ddd';
-            const percentOfVote = `${Math.round(c.votes/totalVotes * 100)}%`;
+            const percentOfVote = Math.round(c.votes/totalVotes * 100);
             return (
-              <section style={{...style, width: percentOfVote, backgroundColor }}>
+              <section
+                style={{
+                  ...style,
+                  backgroundColor,
+                  backgroundSize: '200% 100%',
+                  backgroundImage: `linear-gradient(to right, white 50%, ${backgroundColor} 50%)`,
+                  transition: 'background-position 1s ease',
+                  backgroundPosition: `${mounted ? `${-percentOfVote}% 0` : '0 0'}`
+                }}>
                 {
                   isUserVote &&
                   <CheckMark
                     style={{verticalAlign: 'middle', display: 'inline-block'}} />
                  }
-                <span style={{marginLeft: 5}}>{ c.text } - { percentOfVote }</span>
+                <span style={{marginLeft: 5}}>{ c.text } - { `${percentOfVote}%` }</span>
               </section>
             )
           })
@@ -35,7 +52,6 @@ class Results extends Component {
 const style = {
   borderRadius: '5px',
   textAlign: 'left',
-  whiteSpace: 'nowrap',
   padding: '5px 0',
   margin: '5px 0'
 };
